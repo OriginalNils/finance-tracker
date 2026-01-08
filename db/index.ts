abd1a5b836b1179bd693880034d65c723ea492b6
OriginalNils <nils-doering@gmx.de>
2026-01-09 00:32:28 +0100

    Fix: Change database host from localhost to LXC IP

--- db/index.ts
+++ db/index.ts
@@ -4,7 +4,7 @@ import { Pool } from 'pg';
 
 // Ändere localhost auf die IP deines Datenbank-Containers (LXC 100)
 const pool = new Pool({
-  connectionString: "postgresql://nils:financepassword@192.168.178.100:5432/finance_db",
+  connectionString: "postgresql://finance_user:financepassword@192.168.178.100:5432/finance_db",
 });
 
 export const db = drizzle(pool);
